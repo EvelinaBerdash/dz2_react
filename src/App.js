152dfd492e896688ca7193bea751b3c8e04d0f81
Eva <skyicelands@gmail.com>
2022-12-22 19:36:13 +0300

    dz2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import { Message } from './message'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-import styles from './message.module.css'
+import styles from './app.module.css'
 
 export function App() {
   const [text, setText] = useState(0)
+  const [messageList, setMessageList] = useState([
+  ])
+
+  const handleClick = () => {
+    let newMessage = {
+      author: 'me', text: text
+    }
+    setMessageList([...messageList, newMessage])
+    setText('')
+  }
 
   const handleChange = (event) => {
     setText(event.target.value)
   }
 
+  useEffect(() => {
+    if (messageList.length && messageList[messageList.length - 1].author === 'me') {
+      let newMessage = {
+        author: 'bot', text: 'bot message'
+      }
+      setMessageList([...messageList, newMessage])
+    }
+    console.log(messageList)
+  });
+
   return (
     <>
       <h1> Hello world!!!</h1>
       <h2 > Text Message: {text}</h2>
-      <input type='text' onChange={handleChange} className={styles.border} />
-      
-      <Message textprops={text} />
+      <input type='text' value={text} onChange={handleChange} className={styles.border} />
+      <button className='btn' onClick={handleClick}>Отправить</button>
+      <ul className={styles.message_list}>
+        {messageList.map((item) => (
+          <li className={styles.message_list__item}>{item.author}: {item.text}</li>
+        ))}
+      </ul>
     </>
   )
 }
